c9c2e08790302f2f019ab8cc0f028efca383753f
Valery Peschanyy <p.v.s.oren@gmail.com>
2018-01-09 18:19:59 +0500

    - init commit;

---
+++ src/main/java/com/example/chess/ChessApplication.java
@@ -0,0 +1,19 @@
+package com.example.chess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ChessApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ChessApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChessApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/chess/ExampleController.java
@@ -0,0 +1,19 @@
+package com.example.chess;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Valery Peschanyy <p.v.s.oren@gmail.com> on 09.01.2018.
+ */
+@RestController
+@RequestMapping(path = "/api")
+public class ExampleController {
+
+    @GetMapping(path = "/version")
+    public ResponseEntity<String> getVersion() {
+        return ResponseEntity.ok("v.0.0.1");
+    }
+}
