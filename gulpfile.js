dce9d92d176a5977403654b7aa5af3645fb3525a
Valery Peschanyy <p.v.s.oren@gmail.com>
2018-01-16 18:05:28 +0500

    - remove gulp plugin (Why would I want it?)

--- gulpfile.js
+++
@@ -1,5 +0,0 @@
-var gulp = require('gulp'); // Require gulp
-
-// Sass dependencies
-var sass = require('gulp-sass'); // Compile Sass into CSS
-var minifyCSS = require('gulp-minify-css'); // Minify the CSS
