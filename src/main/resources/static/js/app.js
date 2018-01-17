45918753ed62633e20242d9f2eebc43138729e64
Valery Peschanyy <p.v.s.oren@gmail.com>
2018-01-17 18:35:11 +0500

    - added spring-boot-dev-tools, sprite; - board example;

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,5 +1,22 @@
 var app = angular.module('app', []);
 
 app.controller('common', function ($scope) {
-  $scope.greeting = {id: 'xxx', content: 'Hello World!'}
+  $scope.url = {
+    board: 'chessboard.html'
+  };
+  $scope.greeting = {id: 'xxx', content: 'Hello World!'};
+
+  $scope.rows = [1, 2, 3, 4, 5, 6, 7, 8];
+  $scope.columns = [1, 2, 3, 4, 5, 6, 7, 8];
+
+
+  $scope.getCellClass = function x(rowIndex, columnIndex) {
+    console.log("rowIndex: " + rowIndex + ", columnIndex: " + columnIndex)
+    if (rowIndex == 4 && columnIndex == 4) {
+      return "pawn";
+    } else {
+      return "";
+    }
+  }
+
 });
